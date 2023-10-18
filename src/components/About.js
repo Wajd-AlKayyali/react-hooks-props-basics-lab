865aaec943f6f641c187f516a2950269eaddfc2e
wajd alkayyali <wajd.kayyali@gmail.com>
2023-10-18 17:04:38 +0300

    Done

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,21 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+  let bioParagraph = null;
+
+  if (props.bio && props.bio !== "") {
+    bioParagraph = <p>{props.bio}</p>;
+  } else {
+    bioParagraph = null;
+  }
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bioParagraph}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links />
     </div>
   );
 }
